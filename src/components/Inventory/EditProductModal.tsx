5d5c311fe4cd78488f06b7fb8eb99333b00e7381
Brandon-Lee Schultz <32305446+brandon-lee-schultz@users.noreply.github.com>
2025-09-16 14:19:50 +0200

    update

--- src/components/Inventory/EditProductModal.tsx
+++ src/components/Inventory/EditProductModal.tsx
@@ -186,7 +186,7 @@ const EditProductModal = ({ open, onOpenChange, product, onSave }: EditProductMo
             <div className="space-y-2">
               <Label htmlFor="currentPrice">Current Price</Label>
               <div className="relative">
-                <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 inline-block border-r border-gray-500 pr-2">
+                <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 inline-block border-r border-gray-500 pr-1">
                   R
                 </span>
                 <Input
